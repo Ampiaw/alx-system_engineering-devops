42dc089a7813ec82587622d3fc1e3f3d064dc4ac
Ebenezer Ampiaw Yeboah <yebenezer64@gmail.com>
2022-12-14 15:11:34 -0800

    task 0 and 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+/**
+ * main - function that prints the lowercase alphabets
+ *
+ * return: 0
+ */
+#include "main.h"
+void print_alphabet(void)
+{
+	char a_l = 'a';
+
+	while (a_l <= 'z')
+	{
+		_putchar(a_l);
+		a_l++;
+	}
+	_putchar('\n');
+	return (0);
+}
