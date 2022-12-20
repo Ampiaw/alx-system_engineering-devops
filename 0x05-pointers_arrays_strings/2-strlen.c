96e2e246c24358d48cdf2a55b66fd8854be5d52b
Ebenezer Ampiaw Yeboah <yebenezer64@gmail.com>
2022-12-19 19:30:36 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - to return the length of string
+ * @s: character
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int counter = 0; /*starting counter from zero*/
+
+	for (; *s++;)
+		counter++;
+	return (0);
+}
