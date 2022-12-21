3cf70a15aaee6051db45657a80af48c91eba1800
Ebenezer Ampiaw Yeboah <yebenezer64@gmail.com>
2022-12-20 20:59:53 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - copies pointed string
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+
+	while (*(src + len) != '\0')
+	{
+		*(dest + len) = *(src + len);
+		len++;
+	}
+
+	*(dest + len) = '\0';
+	return (dest);
+}
